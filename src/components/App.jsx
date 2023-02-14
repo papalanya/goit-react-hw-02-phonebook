5e5b979e77b2a03b0a50f7581ce59b7b8b168470
Illia Pavlenko <illiampavlenko@gmail.com>
2023-02-14 04:17:33 +0200

    add step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,41 @@
 import React, { Component } from 'react';
 
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+
 import { nanoid } from 'nanoid';
 
+
+export class App extends Component {
+    state = {
+        contacts: [],
+        name: ''
+      }
+
+    formSubmitHandler = ({ name }) => {
+        const newContact = {
+            id: nanoid(),
+            name,
+        };
+
+        this.setState(prevState => ({
+            contacts: [...prevState.contacts, newContact],
+        }));
+    };
+
+    render() {
+        const { contacts } = this.state;
+        return (
+            <div>
+                <h1>Phonebook</h1>
+                <ContactForm onData={this.formSubmitHandler} />
+
+                <h2>Contacts</h2>
+                <ContactList contacts={contacts} />
+            </div>
+        );
+    }
+}
 
 
 
